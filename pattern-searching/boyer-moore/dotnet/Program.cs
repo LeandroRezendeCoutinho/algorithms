84fa3d96a51437a67f5af6bb3ad053a37c90e894
leandrorezendecoutinho <leandro.rc.contato@gmail.com>
2021-11-24 09:07:09 -0300

    add string pattern search boyer moore algorithm

---
+++ pattern-searching/boyer-moore/dotnet/Program.cs
@@ -0,0 +1,11 @@
+﻿
+public class Program
+{
+    public static void Main()
+    {
+        char[] txt = "ABAAABCD".ToCharArray();
+        char[] pat = "ABC".ToCharArray();
+        BoyerMoore.search(txt, pat);
+    }
+
+}
